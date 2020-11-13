ee6bcd649b2f8aeed15a3e43b7c885208f042d5d
robrichardsdpt <robrichardsdpt@gmail.com>
2020-11-13 18:52:38 -0500

    User can now like comments.

--- src/components/GratFeed/GratFeed.js
+++ src/components/GratFeed/GratFeed.js
@@ -31,6 +31,7 @@ class GratFeed extends React.Component {
         gratitude_likes: 0
       },
       gratitude_likes: [],
+      comment_likes: [],
       showEdit: false,
       showDelete: false,
       showComment: false,
@@ -42,6 +43,11 @@ class GratFeed extends React.Component {
         owner: this.props.user.id,
         gratitude: 0
       },
+      comment_like: {
+        like: 0,
+        owner: this.props.user.id,
+        comment: 0
+      },
       comment: {
         text: '',
         owner: this.props.user.id,
@@ -50,7 +56,186 @@ class GratFeed extends React.Component {
       },
       comments: [],
       showCommentEdit: false,
-      editCommentId: ''
+      editCommentId: '',
+      createdCommentlikeId: ''
+    }
+  }
+
+  commentBasedOnId = (id, comments) => {
+    for (let i = 0; i < comments.length; i++) {
+      console.log(id, comments[i].id)
+      const intId = parseInt(id)
+      const commentId = parseInt(comments[i].id)
+      if (commentId === intId) {
+        this.setState({
+          comment_like: {
+            comment: commentId
+          }
+        })
+        return comments[i]
+      }
+    }
+  }
+
+  findCommentLike = (comment, owner) => {
+    let commentLike
+    for (let i = 0; i < this.state.comment_likes.length; i++) {
+      if (this.state.comment_likes[i].owner === parseInt(owner) && this.state.comment_likes[i].comment === parseInt(comment)) {
+        commentLike = this.state.comment_likes[i]
+        return commentLike
+      }
+    }
+    return false
+  }
+
+  onCommentLike = (event) => {
+    console.log(this.state.comments)
+    const commentClick = event.target.name
+    let comment = {}
+    comment = this.commentBasedOnId(commentClick, this.state.comments)
+    const commentLikesArray = comment.likes
+    const user = this.state.user.id
+    let commentLike = {}
+    commentLike = this.findCommentLike(commentClick, this.state.user.id)
+    // gratLike tells you that the owner has a like for that gratitude already and sends it to be deleted
+    if (commentLikesArray.length !== 0 && commentLike) {
+      this.setState({
+        comment_like:
+        { like: 0,
+          owner: user,
+          comment: comment }
+      })
+      return axios({
+        url: `${apiUrl}/comment_likes/${commentLike.id}`,
+        method: 'DELETE',
+        headers: {
+          Authorization: 'Token ' + `${this.state.user.token}`
+        }
+      })
+        .then((response) => {
+          return axios({
+            url: `${apiUrl}/comments/`,
+            method: 'GET',
+            headers: {
+              Authorization: 'Token ' + `${this.state.user.token}`
+            }
+          })
+        })
+        .then(response => {
+          this.setState({
+            comments: response.data.comments
+          })
+          return axios({
+            url: `${apiUrl}/comment_likes/`,
+            method: 'GET',
+            headers: {
+              Authorization: 'Token ' + `${this.state.user.token}`
+            }
+          })
+        })
+        .then(response => {
+          this.setState({
+            comment_likes: response.data.comment_likes
+          })
+        })
+    } else if (commentLikesArray.length !== 0 && !commentLike) {
+      const cLCopy = Object.assign({}, this.state.comment_like) // to get the original state of the run and to copy it into another object to bypass inability to assign to a state
+      // Object.assign({}, object-to-copy) allows you to combine two objects
+      // updating the key in our state with what the user typed in
+      cLCopy['owner'] = this.state.user.id
+      cLCopy['comment'] = commentClick
+      this.setState({
+        comment_like: cLCopy
+      })
+      console.log(cLCopy)
+      return axios({
+        url: `${apiUrl}/comment_likes/`,
+        method: 'POST',
+        headers: {
+          Authorization: 'Token ' + `${this.state.user.token}`
+        },
+        data: {
+          comment_like: cLCopy
+        }
+      })
+        .then((response) => {
+          this.setState({
+            createdCommentlikeId: response.data.comment_like.id
+          })
+          return axios({
+            url: `${apiUrl}/comments/`,
+            method: 'GET',
+            headers: {
+              Authorization: 'Token ' + `${this.state.user.token}`
+            }
+          })
+            .then(response => {
+              this.setState({
+                comments: response.data.comments
+              })
+              return axios({
+                url: `${apiUrl}/comment_likes/`,
+                method: 'GET',
+                headers: {
+                  Authorization: 'Token ' + `${this.state.user.token}`
+                }
+              })
+                .then(response => {
+                  this.setState({
+                    comment_likes: response.data.comment_likes
+                  })
+                })
+            })
+        })
+    } else if (commentLikesArray.length === 0 && !commentLike) {
+      const cLCopy = Object.assign({}, this.state.comment_like) // to get the original state of the run and to copy it into another object to bypass inability to assign to a state
+      // Object.assign({}, object-to-copy) allows you to combine two objects
+      // updating the key in our state with what the user typed in
+      cLCopy['owner'] = this.state.user.id
+      cLCopy['comment'] = commentClick
+      this.setState({
+        comment_like: cLCopy
+      })
+      console.log(cLCopy)
+      return axios({
+        url: `${apiUrl}/comment_likes/`,
+        method: 'POST',
+        headers: {
+          Authorization: 'Token ' + `${this.state.user.token}`
+        },
+        data: {
+          comment_like: cLCopy
+        }
+      })
+        .then((response) => {
+          this.setState({
+            createdCommentlikeId: response.data.comment_like.id
+          })
+          return axios({
+            url: `${apiUrl}/comments/`,
+            method: 'GET',
+            headers: {
+              Authorization: 'Token ' + `${this.state.user.token}`
+            }
+          })
+            .then(response => {
+              this.setState({
+                comments: response.data.comments
+              })
+              return axios({
+                url: `${apiUrl}/comment_likes/`,
+                method: 'GET',
+                headers: {
+                  Authorization: 'Token ' + `${this.state.user.token}`
+                }
+              })
+                .then(response => {
+                  this.setState({
+                    comment_likes: response.data.comment_likes
+                  })
+                })
+            })
+        })
     }
   }
 
@@ -698,7 +883,7 @@ class GratFeed extends React.Component {
               </div>
               <br/>
             </div>
-            <div className="col-9 heart" ><Button variant="primary" size='sm' className='heartbtn' name={comment.id} onClick={console.log('like')}>Like</Button>{comment.likes.length}</div>
+            <div className="col-9 heart" ><Button variant="primary" size='sm' className='heartbtn' name={comment.id} onClick={this.onCommentLike}>Like</Button>{comment.likes.length}</div>
           </div>
         )
       })
